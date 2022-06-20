c9072223ce7a812a2e6d1f434d9e77b1f432c246
kilelx <llx.kieran@gmail.com>
2022-06-20 21:14:42 +0200

    Update animations header

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,14 @@
 const header = document.querySelector("header");
-const burgerToggle = document.querySelector("#toggle-burger");;
-const animLis = document.querySelectorAll("header .navbar_content > li");
+const burgerToggle = document.querySelector("#toggle-burger");
+const heroSection = document.querySelector(".hero-content");
+// const animLis = document.querySelectorAll("header .navbar_content > li");
+
+console.log(header.offsetHeight);
 
 burgerToggle.addEventListener("click", () => {
     header.classList.toggle("active");
-    animLis.forEach(li => {
-        li.classList.toggle("run-transition");
-    });
+    heroSection.style.paddingTop = `${header.offsetHeight}px`;
+    // animLis.forEach(li => {
+    //     li.classList.toggle("run-transition");
+    // });
 });
